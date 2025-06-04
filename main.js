1ea3bb7d7d8689e5067b0a713414069a122ef705
Vika <vikacaramelka14@gmail.com>
2025-06-04 19:12:05 -0400

    html work

---
+++ main.js
@@ -0,0 +1,14 @@
+// Make two variables
+//You must change to a string to use two 0's
+var minutes = 25;
+var seconds = "00";
+
+// Create function to connect id from index.html to variables
+function template() {
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+
+function start() {
+    
+}
